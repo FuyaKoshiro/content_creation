dd9852bb59d20e76cf28f39eb0cc1bf7211e4633
FuyaKoshiro <s.castle28546@gmail.com>
2023-03-28 19:25:12 -0700

    updated

--- extract_db.py
+++ extract_db.py
@@ -1,21 +1,24 @@
-# extract the all vtitle and vcode dataframe from the db.sqlite3
+"""
+==================
+extract the all vtitle and vcode dataframe from the db.sqlite3
+==================
+"""
+
+
 import sqlite3
 import pandas as pd
 
 
 class ExtractDB:
-    def __init__(self):
-        self.vtitle_list_all = []
-        self.conn = sqlite3.connect("db.sqlite3")
-        self.c = self.conn.cursor()
-        self.df = None
 
     def get_df(self):
-        self.c.execute("SELECT video_code, video_title FROM video_db")
-        self.df = pd.DataFrame(self.c.fetchall(), columns=["vcode", "vtitle"])
-        print(self.df)
-        return self.df
+        conn = sqlite3.connect("db.sqlite3")
+        c = conn.cursor()
+        c.execute("SELECT video_code, video_title FROM video_db")
+        df = pd.DataFrame(self.c.fetchall(), columns=["vcode", "vtitle"])
+        print(df)
+        return df
 
 if __name__ == "__main__":
-    edb = ExtractDB()
-    df = edb.get_df()
+    extract_db = ExtractDB()
+    df = extract_db.get_df()
